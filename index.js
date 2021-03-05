d99f29da857609eb755187306188423589762970
shikhars371 <shikhars371@gmail.com>
2021-03-05 11:29:17 +0530

    var changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const p = require('path')
 const API_VERSION = 0
 
 module.exports = {
-  hyperdriveSchemaPath: p.join(__dirname, 'schemas', 'hyperdrive.proto'),
+  ddriveSchemaPath: p.join(__dirname, 'schemas', 'ddrive.proto'),
   messages: require('./lib/messages'),
   Stat: require('./lib/stat'),
   version: API_VERSION
